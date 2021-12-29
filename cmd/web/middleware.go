42e25d6378034c1ad6f5ac9058bbd7861db313da
Movsar Makhmutov <m.movsar@gmail.com>
2021-12-29 19:39:24 +0300

    added nosurf

---
+++ cmd/web/middleware.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"github.com/justinas/nosurf"
+	"net/http"
+)
+
+func WriteToConsole(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Println("Hit the page")
+		next.ServeHTTP(writer, request)
+	})
+}
+
+func NoSurf(next http.Handler) http.Handler {
+	csrfHandler := nosurf.New(next)
+	csrfHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,
+		Path:     "/",
+		Secure:   false,
+		SameSite: http.SameSiteLaxMode,
+	})
+	return csrfHandler
+}
