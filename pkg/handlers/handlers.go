56ec5e45ff54fb906a7932f081e54381f4568d4c
Movsar Makhmutov <m.movsar@gmail.com>
2021-12-29 21:38:53 +0300

    Displayed IP in html

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -32,6 +32,8 @@ func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	stringMap := make(map[string]string)
 	stringMap["test"] = "Hello again"
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIP
 	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
 		StringMap: stringMap,
 	})
