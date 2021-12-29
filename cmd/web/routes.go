527ea48a4f4c7be959f3b907818404023ef4bb96
Movsar Makhmutov <m.movsar@gmail.com>
2021-12-29 18:56:36 +0300

    added external library for routing

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,15 +1,19 @@
 package main
 
 import (
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"go-hello-world/pkg/config"
 	"go-hello-world/pkg/handlers"
 	"net/http"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 	return mux
 }
